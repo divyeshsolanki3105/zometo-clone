9dafd4bed154d592d3fd775d9889cc2027c34d56
divyesh solanki <you@example.com>
2023-02-22 18:11:34 +0530

    first commit

---
+++ city.js
@@ -0,0 +1,40 @@
+  export const city=[
+    {
+        name:"Lower parel",
+        place:383
+    },
+    {
+        name:"Powai",
+        place:361
+    },
+    {
+        name:"Malad west",
+        place:799
+    },
+    {
+        name:"Borivali west",
+        place:566
+    },
+    {
+        name:"Bandra Kurla Complex",
+        place:97
+    },
+    {
+        name:"Vashi",
+        place:541
+    },
+    {
+        name:"Fort",
+        place:220
+    },
+    {
+        name:"juhu",
+        place:197
+    }, 
+    {
+        name:"see more",
+        place:"",
+        class: "seeMore"
+    },
+]
+//export default city;
